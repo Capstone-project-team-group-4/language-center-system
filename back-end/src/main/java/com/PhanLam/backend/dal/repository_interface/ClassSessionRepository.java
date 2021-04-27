b2c7aad3ca1a2ef5fd882d48635d083f6921f381
lydinhnhonnghia <nghialdnse05508@fpt.edu.vn>
2021-04-27 23:08:56 +0700

    search class

--- back-end/src/main/java/com/PhanLam/backend/dal/repository_interface/ClassSessionRepository.java
+++ back-end/src/main/java/com/PhanLam/backend/dal/repository_interface/ClassSessionRepository.java
@@ -6,8 +6,9 @@ import com.PhanLam.backend.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ClassSessionRepository extends JpaRepository<ClassSession, Integer> {
+public interface ClassSessionRepository extends JpaRepository<ClassSession, Integer>, JpaSpecificationExecutor<ClassSession> {
     Page<ClassSession> findAllByTeacherID(Pageable pagingInformation, User teacher);
 
     ClassSession findByTeacherIDAndSlot(User user, Slot slot);
